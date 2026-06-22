0688266f7f005bc6dc23c39312a11742a087f50c
pramodGit <pramodkumar.email@gmail.com>
2026-06-22 20:28:53 +0530

    Promise and Async conceptual description

--- src/app/components/JavaScript/promiseAsync.component.ts
+++ src/app/components/JavaScript/promiseAsync.component.ts
@@ -11,32 +11,58 @@ import { RouterLink } from '@angular/router';
 export class PromiseAsync {
     codeSnippet1 = `
     const promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve(100), 300);
+        setTimeout(resolve(100), 300);
+        //setTimeout(reject(0), 400);
     });
-    promise.then((result) => {
-        console.log(result);
-    return 200;
+    promise
+    .then((result) => {
+        console.log(\`First "then" & Promise result => \${result}\`);
+        return 300;
     })
     .then((result) => {
-        setTimeout(() => console.log(result), 300);
-    return 300;
+        setTimeout(() => { console.log(\`Second "then" & Promise result => \${result}\`);}, 200);
+        return 200;
     })
-    .then((result) => console.log(result));`;
+    .then(result => console.log(\`Third "then" & Promise result => \${result}\`))
+    .catch(error => console.error('Error:', error)); // Error handling
+
+    // Console Result
+
+    First "then" & Promise result => 100
+    Third "then" & Promise result => 200
+    Second "then" & Promise result => 300
+    `;
 
     codeSnippet2 = `
     const func = async () => {
-        let promise1 = new Promise((resolve, reject) => {
-            resolve(100);
-        });
-        let promise2 = new Promise((resolve, reject) => {
-            setTimeout(() => resolve(200), 10);
-        });
-        let promise3 = new Promise((resolve, reject) => {
-            resolve(300);
-        });
-        await console.log(promise1);
-        await console.log(promise2);
-        await console.log(promise3);
+        try {
+            let promise1 = new Promise((resolve) => {
+                resolve(100);
+            });
+
+            let promise2 = new Promise((resolve) => {
+                setTimeout(() => resolve(200), 1000);
+            });
+
+            let promise3 = new Promise((resolve) => {
+                setTimeout(() => resolve(300), 100);
+            });
+
+            console.log(await promise1);
+            console.log(await promise3);
+            console.log(await promise2);
+
+        } catch (error) {
+            console.log("Error:", error); // Error handling
+        }
     };
-    func();`;
+
+    func();
+    
+    // Console result
+
+    100
+    300
+    200
+    `;
 }
